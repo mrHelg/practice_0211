9f16fe6dc80370e1a5d124fa870e407a2a9a317f
Som&soft <som8soft@gmail.com>
2021-11-02 18:14:31 +0200

    Final version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
+const pattern = /[^\<\>\:"\/\\\|\?\*]/;
 
+const form = document.getElementById('form');
+const inputText = document.getElementById('inputText');
+const btn = document.getElementById('btn');
+
+const filenamePattern = /^[^<>:?"/*|\\]+(.png|.jpg)$/;
+
+function checkFileName(e){
+  e.preventDefault();
+  if (filenamePattern.test(inputText.value)) {
+    alert('File name is corect');
+  } else {
+    alert('Error');
+  }
+  form.reset();
+}
+
+btn.addEventListener('click', checkFileName);
